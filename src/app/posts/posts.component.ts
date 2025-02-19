fd9e00932d442746d38e10b3cd6101d4cbbdacde
Julian Secades <julian.secades@a.com>
2025-02-19 16:40:02 +0100

    moved post service to services folder

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PostService } from '../post.service';
+import { PostService } from '../services/post.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
