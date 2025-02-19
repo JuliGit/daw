ccdcbe64ff6fdf796376b018fefe8fa1bf0d2739
Julian Secades <julian.secades@a.com>
2025-02-19 14:35:25 +0100

    Added component and service post

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { PostService } from '../post.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-posts',
+  imports: [CommonModule],
+  templateUrl: './posts.component.html',
+  styleUrl: './posts.component.css'
+})
+export class PostsComponent {
+  posts: any[] = [];
+
+  constructor(private postService:PostService) { }
+
+  ngOnInit() {
+    this.postService.getPosts().subscribe((data:any[]) => {
+      console.log(data);
+      this.posts = data;
+    });
+  }
+
+}
